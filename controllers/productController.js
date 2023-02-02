bee6b388bb6071b8f15aa487c5cb4a186dbc9176
LuiyiBarrantes <luiyibarrantes@gmail.com>
2023-02-02 04:11:27 -0300

    add metodo create incompleto en productController

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,6 @@
+const products=require('../data/products.json')
+const fs=require('fs')
+
 module.exports={
    products: (req,res) =>{
         return res.render('products', {
@@ -8,5 +11,15 @@ module.exports={
         return res.render('productDetail', {
             title: 'Detalle de Producto'
         });
+    },
+    create: (req,res)=>{
+        res.render('create',{
+            title: 'Crear Producto'
+        });
+        let productsRead=fs.readFileSync('../data/products.json','utf-8');
+        let productsJson=JSON.parse(productsRead)
+        fs.writeFileSync('../data/products.json',JSON.stringify(products))
+        res.redirect('products')
     }
+    
 }
