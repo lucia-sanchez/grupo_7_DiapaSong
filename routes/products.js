3d2f24e41daeddf5235a9e340c5b6d2e8552f1c0
LuiyiBarrantes <luiyibarrantes@gmail.com>
2023-02-22 01:16:26 -0300

    agrega middleware upload en products routes

--- routes/products.js
+++ routes/products.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const{detail,products,create,saveCreate,edit,update,removeConfirm,remove} = require('../controllers/productController');
+const { uploadProductsImage } = require('../middlewares/upload');
 
 /* /products */
 router.get('/', products)
       .get('/detail/:id?', detail)
       .get('/create', create)
-      .post('/create', saveCreate)
+      .post('/create',uploadProductsImage.fields([{name:'mainImage'},{name:'images'}]), saveCreate)
       .get('/edit/:id',edit)
       .put('/update/:id',update)
       .get('/remove/:id',removeConfirm)
