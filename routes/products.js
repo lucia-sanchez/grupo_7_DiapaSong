95a5d9e7cd440aed6eecd8e6be4d356be60557cd
Maximiliano <elgrungemaxi1986@gmail.com>
2023-03-13 22:41:51 +0100

    agregue productos en categorias

--- routes/products.js
+++ routes/products.js
@@ -3,16 +3,17 @@ const router = express.Router();
 
 
 const{detail,products,create,saveCreate,edit,update,removeConfirm,remove} = require('../controllers/productController');
+// const checkUserAdmin = require('../middlewares/checkUserAdmin');
 const { uploadProductsImage } = require('../middlewares/upload');
 
 /* /products */
 router.get('/', products)
       .get('/detail/:id?', detail)
-      .get('/create', create)
-      .post('/create',uploadProductsImage.fields([{name:'mainImage'},{name:'images'}]), saveCreate)
-      .get('/edit/:id',edit)
+      .get('/create', /*checkUserkAdmin,*/ create)
+      .post('/create', uploadProductsImage.fields([{name:'mainImage'},{name:'images'}]), saveCreate)
+      .get('/edit/:id', /*checkUserAdmin,*/ edit)
       .put('/update/:id', uploadProductsImage.fields([{name:'mainImage'},{name:'images'}]),update)
-      .get('/remove/:id',removeConfirm)
+      .get('/remove/:id', /*checkUserAdmin,*/ removeConfirm)
       .delete('/remove/:id', remove )
       .get('/:category?', products)
       
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 const {register,login,password,saveRegister} = require('../controllers/userController');
+const checkUserGuest = require('../middlewares/checkUserGuest');
 /* users */
 router
-        .get('/register', register)
+        .get('/register', /*checkUserGuest,*/ register)
         .post('/register', saveRegister)
-        .get('/login', login)
+        .get('/login', /*checkUserGuest,*/ login)
         .get('/password', password)
 
 module.exports = router;
