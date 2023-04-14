dda5aaf5345a21535193c0e88be6ed2f96024fd5
LuiyiBarrantes <luiyibarrantes@gmail.com>
2023-04-14 04:40:59 -0300

    arregla referencia de tabla en userid

--- database/migrations/20230411160010-create-user-genre.js
+++ database/migrations/20230411160010-create-user-genre.js
@@ -14,7 +14,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         references : {
           model :{
-            tableName : "Rols"
+            tableName : "Users"
           },
           key : 'id'
         }
