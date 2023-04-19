8e299b89008be7f53d53538f89054402ba951485
LuiyiBarrantes <luiyibarrantes@gmail.com>
2023-04-19 00:41:09 -0300

    arregla ruta savecreate con multer y validator

--- routes/products.js
+++ routes/products.js
@@ -6,16 +6,17 @@ const{detail,products,create,saveCreate,edit,update,remove} = require('../contro
 const checkUserAdmin = require('../middlewares/checkUserAdmin');
 
 const { uploadProductsImage } = require('../middlewares/upload');
+const productValidator = require('../validations/productValidator');
 
 /* /products */
 router.get('/', products)
       .get('/detail/:id?', detail)
       .get('/create',  create)
       //checkUserAdmin,
-      .post('/create', uploadProductsImage.fields([{name:'mainImage'},{name:'images'}]), saveCreate) 
+      .post('/create', uploadProductsImage, productValidator, saveCreate) 
       .get('/edit/:id', edit)
       //checkUserAdmin,
-      .put('/update/:id', uploadProductsImage.fields([{name:'mainImage'},{name:'images'}]),update)
+      .put('/update/:id', uploadProductsImage/* .fields([{name:'mainImage'},{name:'images'}]) */,update)
       .delete('/remove/:id',remove )
       //checkUserAdmin, 
       .get('/:category?', products) 
