6ee4c9a511ecb3e34c4b4f798601313a958560a9
Maximiliano <Elgrungemaxi1986@gmail.com>
2023-05-02 20:23:05 -0300

    implemente recomendacion al menu categorias de productos

--- public/javascript/dinamic.js
+++ public/javascript/dinamic.js
@@ -1,18 +1,18 @@
 //Scroll//
 window.addEventListener("scroll", function () {
     const main = document.querySelector("main");
-    main.classList.toggle("scroll_main_space_top", window.scrollY > 1);
-});
-window.addEventListener("scroll", function () {
     const header = document.querySelector("header");
-    header.classList.toggle("header-scroll", window.scrollY > 1);
+    const navBar = document.querySelector(".menufixed");
+      main.classList.toggle("scroll_main_space_top", window.scrollY > 1);
+      header.classList.toggle("header-scroll", window.scrollY > 1);
+      navBar.classList.toggle("fixed", window.scrollY > 1);
+      
 });
+
 
 //Menu desplegable header//
 const btnmenu = document.querySelector("#iconomenu");
-
 const menu = document.querySelector("#header_nav");
-
 btnmenu.addEventListener("click", function () {
     menu.classList.toggle("mostrar-menu")
 })
