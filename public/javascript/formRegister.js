b7a1bcdecf4a0ed7cb43de4beaeaedf99da8ce62
Lucia Sanchez <92888197+lucia-sanchez@users.noreply.github.com>
2023-05-12 21:41:27 -0300

    validacion de imagen y DNI

--- public/javascript/formRegister.js
+++ public/javascript/formRegister.js
@@ -51,14 +51,14 @@ let regExEmail =
 $('name').addEventListener('blur', function(e){
 switch (true) {
     case !this.value.trim():
-        msgError('errorName', 'El nombre es obligatorio', e)  
+        msgError('error-name', 'El nombre es obligatorio', e)  
         break;
 
     case this.value.trim().length < 2 || this.value.trim().length > 50:
-        msgError('errorName', "Entre 2 y 50 caracteres",e)
+        msgError('error-name', "Entre 2 y 50 caracteres",e)
           break
     case !regExLetter.test(this.value.trim()):
-        msgError('errorName', "Solo caracteres alfabeticos",e)
+        msgError('error-name', "Solo caracteres alfabeticos",e)
           break
     default:
         this.classList.add('validInput')
@@ -68,8 +68,33 @@ switch (true) {
     });
   
     $('name').addEventListener('focus', function(e) {
-      cleanError('errorName', e)
+      cleanError('error-name', e)
     });
+
+    //IMAGE VALIDATION
+
+    const regExExt = /(.jpg|.jpeg|.png|.gif|.webp)$/i;
+
+    $('mainImage').addEventListener('change', function(e){
+      switch (true) {
+          case !regExExt.exec(this.value):
+              $('error-image').innerHTML = "Solo se admiten imágenes jpg | jpeg | png | webp"
+              break;
+          case this.isDefaultNamespace.length > 1 :   
+          $('error-image').innerHTML = "Solo una imagen"
+          break
+          default:
+              this.classList.add('validInput')
+              checkedFields()
+              break;
+      }
+    });
+    $('mainImage').addEventListener('focus', function(e) {
+      cleanError('error-image', e)
+    })
+    
+  
+  
 
     //EMAIL VALIDATION
     $("email").addEventListener("blur", async function (e) {
@@ -93,6 +118,7 @@ switch (true) {
     $("email").addEventListener("focus", function (e) {
       cleanError("error-email", e);
     });
+
     //PASSWORD VALIDATION
     $("password").addEventListener("blur", function (e) {
       $("msgPassword").hidden = true;
@@ -104,7 +130,7 @@ switch (true) {
         case !regExPass2.test(this.value.trim()):
           msgError(
             "error-password",
-            "Debe ser entre 6 y 12 caracteres y tener una mayúscula, una minúscula y un número",
+            "Debe ser entre 6 y 12 caracteres y tener una mayúscula, una minúscula y un número y un caracter especial",
             e
           );
           break;
@@ -124,7 +150,7 @@ switch (true) {
       exRegMayu: /[A-Z]/,
       exRegMinu: /[a-z]/,
       exRegNum: /[0-9]/,
-      exRegEsp: /[$@!%*?&_-]/,
+      exRegEsp: /[$@!%*?&]/,
       exRegMin: /.{6,}/,
       exRegMax: /.{13}/,
     };
@@ -148,11 +174,91 @@ switch (true) {
     };
     
     $("password").addEventListener("keyup", function () {
-      validPassword("mayu", exRegs.exRegMayu, this.value);
-      validPassword("minu", exRegs.exRegMinu, this.value);
+      validPassword("mayus", exRegs.exRegMayu, this.value);
+      validPassword("minus", exRegs.exRegMinu, this.value);
       validPassword("num", exRegs.exRegNum, this.value);
       validPassword("esp", exRegs.exRegEsp, this.value);
       validPassword("min", exRegs.exRegMin, this.value);
       validMaxPassword("max", exRegs.exRegMax, this.value);
     });
+
+    //CONFIRMACION PASSWORD VALIDATION
+    $("password2").addEventListener("blur", function (e) {
+      switch (true) {
+        case !this.value.trim():
+          msgError("error-password2", "Debes confirmar el password", e);
+          break;
+        case this.value.trim() !== $("password").value.trim():
+          msgError("error-password2", "La confirmación no coincide", e);
+          break;
+        default:
+          this.classList.add("is-valid");
+          checkedFields();
+          break;
+      }
+    });
     
+    $("password2").addEventListener("focus", function (e) {
+      cleanError("error-password2", e);
+    });
+     
+    //DNI VALIDATION
+
+    $('identifyid').addEventListener('blur', function(e){
+      switch (true) {
+          case !this.value.trim():
+              msgError('error-identifyid', 'El DNI es obligatorio', e)  
+              break;
+      
+          case this.value.trim().length < 8|| this.value.trim().length > 9:
+              msgError('error-identifyid', "Entre 8 y 9 caracteres",e)
+                break;
+          case !regExNum.test(this.value.trim()):
+              msgError('error-identifyid', "Ingrese su número de DNI sin puntos, solo carácteres numéricos",e)
+                break
+          default:
+              this.classList.add('validInput')
+              checkedFields()
+                break;
+            }
+          });
+        
+          $('identifyid').addEventListener('focus', function(e) {
+            cleanError('error-identifyid', e)
+          });
+      
+
+
+
+
+
+
+    //TERMS VALIDATION
+    $("terms").addEventListener("click", function (e) {
+      this.classList.remove("is-invalid");
+      $("error-terms").innerHTML = null;
+    });
+    
+    $("formRegister").addEventListener("submit", function (e) {
+      e.preventDefault();
+      let error = false;
+    
+      if (!$("terms").checked) {
+        error = true;
+        $("error-terms").innerHTML = "Debes aceptar las bases y condiciones";
+        $("terms").classList.add("is-invalid");
+      }
+    
+      for (let i = 0; i < this.elements.length - 2; i++) {
+        if (
+          !this.elements[i].value.trim() ||
+          this.elements[i].classList.contains("is-invalid")
+        ) {
+          error = true;
+          this.elements[i].classList.add("is-invalid");
+          $("error-form").innerHTML = "Hay campos con errores o están vacíos";
+        }
+      }
+    
+      !error && this.submit();
+    });
