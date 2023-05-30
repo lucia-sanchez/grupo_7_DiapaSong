f65499856d7bb5b07f3b10668812640ca6f43639
Maximiliano <Elgrungemaxi1986@gmail.com>
2023-05-30 09:52:07 -0300

    implemente imagenes secundarias

--- public/javascript/productDetail.js
+++ public/javascript/productDetail.js
@@ -1,12 +1,9 @@
+let productImage = document.querySelector("#product-image");
+let imageView = document.querySelector(".imageView");
 
-let productImage = document.querySelector("#product-image")
-
-
-productImage.addEventListener("click", () =>{
-    productImage.classList.add("imageView")
-})
-let imageView = document.querySelector(".imageView")
-
-imageView.addEventListener("click", () =>{
-    imageView.classList.remove("imageView")
-})
+productImage.addEventListener("click", () => {
+  productImage.classList.toggle("imageView");
+});
+imageView.addEventListener("click", () => {
+  productImage.classList.toggle("imageView");
+});
