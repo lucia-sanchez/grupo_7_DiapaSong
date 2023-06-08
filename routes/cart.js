329ed59739f6c53f1ac53e1919b5344bd7e172d1
LuiyiBarrantes <luiyibarrantes@gmail.com>
2023-06-08 04:17:01 -0300

    agrega rutas

--- routes/cart.js
+++ routes/cart.js
@@ -2,7 +2,7 @@ var express = require('express');
 //const {index, search, blog,saveBlog, faq, suscripcion} = require('../controllers/indexController');
 var router = express.Router();
 const db = require('../database/models');
-const { productCart, addToCart } = require('../controllers/cartController');
+const { productCart, addToCart, spinner, removeFromCart, moreUnits, lessUnits, spinner2, emptyCart } = require('../controllers/cartController');
 const sequelize = db.sequelize;
 
 /* / */
@@ -12,6 +12,12 @@ router
     //.get('/search', search)
     .get('/productCart', productCart)
     .post("/addToCart", addToCart)
+    .delete('/removeFromCart/:id',removeFromCart)
+    .get('/spinner', spinner)
+    .get('/spinner2', spinner2)
+    .patch('/increment/:id',moreUnits)
+    .patch('/decrement/:id',lessUnits)
+    .delete('/emptyCart/:id',emptyCart)
     //.get('/blog', blog)
     //.post('/blog', saveBlog)
 
